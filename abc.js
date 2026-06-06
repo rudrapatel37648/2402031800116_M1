bf0020302d278f98158c52fe81972b384097e6af
rudrapatel37648 <rudrapatel37648@gmail.com>
2026-06-06 11:22:39 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,33 @@
+        // alert("hey there is a alert message !")
+
+
+        // let a=10;
+        // console.log(a)
+
+        // a=100;
+        // console.log(a)
+
+// let plus = document.getElementById("plus");
+// let minus = document.getElementById("minus");
+// let multi = document.getElementById("multi");
+// let divide = document.getElementById("divide");
+// let variables = document.getElementById("variables");
+
+
+// let a = 10;
+// let b = 7;
+
+// variables.innerHTML=`a = ${a}, b = ${b}`
+// plus.innerHTML=`a+b : ${a+b}`
+// minus.innerHTML=`a-b : ${a-b}`
+// multi.innerHTML=`a*b : ${a*b}`
+// divide.innerHTML=`a/b : ${a/b}`
+
+
+
+// var a = 20;
+// if(a>=18) alert("elegible for vote ")
+// else alert("not elegible for vote")
+
+
+
